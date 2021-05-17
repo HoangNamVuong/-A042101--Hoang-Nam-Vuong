6fb356d51b27083c5155b82136d47a112aa615c9
A0421I1 - Hoàng Nam Vương <hoangnamvuong10121995@gmail.com>
2021-05-17 12:06:08 +0700

    vuong update file:7. variable_data type_operator

---
+++ A0421I1- Hoang Nam Vuong/Module 1/7. variable_data type_operator/practice/p_use_operator.js
@@ -0,0 +1,15 @@
+var a;
+ a = prompt("input the width:");
+var b;
+ b = prompt("input the height:");
+var area;
+area = a * b;
+document.write("the area is: " + area);
+//  let inputWidth;
+//  let inputHeight;
+//  inputWidth = prompt("Enter the width");
+//  inputHeight = prompt("Enter the height");
+//  let width = parseInt(inputWidth);
+//  let height = parseInt(inputHeight);
+//  let area = width * height;
+//  document.write("The area is: " + area);
