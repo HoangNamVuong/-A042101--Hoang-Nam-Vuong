7943d9815a597e1552e8bf4bb0243c8069f2ce61
A0421I1 - Hoàng Nam Vương <hoangnamvuong10121995@gmail.com>
2021-08-11 18:17:25 +0700

    update Module 2

--- A0421I1- HoangNamVuong/Module 2/src/lesson07_abstract_class_and_interface/exercise/interface_resizeable_for_geometry/ResizeableCircleTest.java
+++ A0421I1- HoangNamVuong/Module 2/src/lesson07_abstract_class_and_interface/exercise/interface_resizeable_for_geometry/ResizeableCircleTest.java
@@ -6,10 +6,10 @@ public class ResizeableCircleTest {
         resizeableCircles[0] = new ResizeableCircle();
         resizeableCircles[1] = new ResizeableCircle(3.5);
         resizeableCircles[2] = new ResizeableCircle(4.5,"black",false);
-
         for (ResizeableCircle resizeableCircle : resizeableCircles) {
             System.out.println(resizeableCircle);
         }
+
         resizeableCircles[0].resize(Math.random() * 99 + 1);
         resizeableCircles[1].resize(Math.random() * 99 +1);
         resizeableCircles[2].resize(Math.random() * 99 +1);
