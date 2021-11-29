062cc2f3b07fcb458e0ad9a0d7e179502fc443fc
A0421I1 - Hoàng Nam Vương <hoangnamvuong10121995@gmail.com>
2021-11-29 10:26:11 +0700

    update file:lesson2_spring_controller

--- a0421i1_hoang_nam_vuong/module_4_springFramework/lesson2_spring_controller/exercise/calculator_spring/src/main/java/calculator/controller/CalculatorController.java
+++ a0421i1_hoang_nam_vuong/module_4_springFramework/lesson2_spring_controller/exercise/calculator_spring/src/main/java/calculator/controller/CalculatorController.java
@@ -1,2 +1,41 @@
-package calculator.controller;public class CalculatorController {
+package calculator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CalculatorController {
+
+    @RequestMapping("/calculate")
+    public String calculator(ModelMap model, @RequestParam(name = "number1",defaultValue = "0") double number1,
+                             @RequestParam(name = "number2",defaultValue = "0") double number2,
+                             @RequestParam(name = "calculation",defaultValue = "") String calculation) {
+        double result = 0;
+        switch (calculation) {
+            case "Addition(+)":
+                result = number1 + number2;
+                calculation = "Addition";
+                break;
+            case "Subtraction(-)":
+                result = number1 - number2;
+                calculation = "Subtraction";
+                break;
+            case "Multiplication(X)":
+                result = number1 * number2;
+                calculation = "Multiplication";
+                break;
+            case "Division(/)":
+                result = number1 / number2;
+                calculation = "Division";
+                break;
+        }
+        model.addAttribute("number1",number1);
+        model.addAttribute("number2", number2);
+        model.addAttribute("calculation", calculation);
+        model.addAttribute("result", result);
+
+        return "calculate";
+    }
 }
